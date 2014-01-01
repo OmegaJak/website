0060b22a553bb639b42bcfd15410333ac1d376c1
William Marrujo <willmarrujo@gmail.com>
2014-01-01 01:06:57 -0600

    PHP Cleanup & SFCave
    fixed ugly code - I hope you have apache installed
    this update will make it much easier to do anything on the site. trust
    me. also, I added a javascript game I made in a day

---
+++ projects.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+	<?php include "head.php" ?>
+	<title>Projects</title>
+    </head>
+    <body>
+	<header>
+	    <?php include "menu.php" ?>
+	    <h1>Projects</h1>
+	    <h2>The fancy work we've all been doing</h2>
+	</header>
+	<article>
+	    <div>
+		<a href="projects/sfcave/"><h3>SFCave</h3></a>
+		<a href="projects/sfcave/"><img src="projects/sfcave/sfcaveimage.png" height="128px" width="128px" /></a>
+		<p>A quick javascript browser game. Hold any key to rise.</p>
+	    </div>
+	    <div>
+		<a href="projects/fractal/"><h3>Fractal</h3></a>
+		<a href="projects/fractal/"><img /></a>
+		<p>A fractal Generator based on the L-system.</p>
+	    </div>
+	</article>
+    </body>
+</html>
