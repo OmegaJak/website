6d4b8020893a5f2b12972d5ce6d647d8b48975cb
William Marrujo <willmarrujo@gmail.com>
2014-01-06 13:58:12 -0600

    Roboto!!!
    not Ubuntu

--- head.php
+++ head.php
@@ -1,6 +1,5 @@
 <!-- fetched documents -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Ubuntu" type="text/css">
-<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Ubuntu+Mono" type="text/css">
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto" type="text/css">
 <!-- local documents -->
 <link rel="stylesheet" href="dependencies/stylesheets/penguin.css" type="text/css">
--- index.php
+++ index.php
@@ -12,18 +12,18 @@
 	<article>
 	    <h2>Featured</h2>
 	    <table>
-	    <tr>
-	    <td>
-		<a href="projects/sfcave/"><h3>SFCave</h3></a>
-		<a href="projects/sfcave/"><img src="projects/sfcave/sfcaveimage.png" height="128px" width="128px" /></a>
-		<p>A quick javascript browser game. Hold any key to rise.</p>
-	    </td>
-	    <td>
-		<a href="projects/fractal/"><h3>Fractal</h3></a>
-		<a href="projects/fractal/"><img src="" height="128" width="128" /></a>
-		<p>A fractal Generator based on the L-system.</p>
-	    </td>
-	    </tr>
+		<tr>
+		    <td>
+			<a href="projects/sfcave/"><h3>SFCave</h3></a>
+			<a href="projects/sfcave/"><img src="projects/sfcave/sfcaveimage.png" height="128px" width="128px" /></a>
+			<p>A quick javascript browser game. Hold any key to rise.</p>
+		    </td>
+		    <td>
+			<a href="projects/fractal/"><h3>Fractal</h3></a>
+			<a href="projects/fractal/"><img src="" height="128" width="128" /></a>
+			<p>A fractal Generator based on the L-system.</p>
+		    </td>
+		</tr>
 	    </table>
 	    <h2>GitHub</h2>
 	    <p>As it just so happens to be, we are doing this project on a <a href="https://github.com/programminpenguins/website">GitHub repository.</a> Come and join! - If you dare.</p>
