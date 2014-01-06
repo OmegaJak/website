30d48d4689d60482c3c46a8bf90932c207cbb1cc
josmcg <joshuawmcgrath@gmail.com>
2014-01-06 12:18:32 -0600

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div id="button"></div>
+	<div class="footer">
+		<div id="button"></div>
+			<div id="container">
+				<div id="cont">
+					<div class="footer_fifths">
+	   					<h3>Penguins</h3>
+					</div>
+				</div>
+			</div>
+</div>
